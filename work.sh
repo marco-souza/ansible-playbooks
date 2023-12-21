770664ec7176369bcde4c9c4245771cf3635a157
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-21 09:16:00 -0300

    fea: add workspace script

---
+++ work.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+session=ansible
+workspace=$HOME/workspace/marco-souza/ansible-playbooks
+
+# kill session
+if [ "$1" = "k" ]; then
+  echo "Kill session"
+  tmux kill-session -t $session
+  exit 0
+fi
+
+echo "Creating workspace..."
+
+# setup windows
+tmux new \
+  -s $session -n $session \
+  -c $workspace -d \
+  nvim
+
